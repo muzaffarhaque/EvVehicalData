3f2276e801d6a0481502b41e1657f9289b1dbc6a
muzaffarhaque <muzaffarhaque777@gmail.com>
2025-03-26 02:32:10 +0530

    subject : gap is not working in safari old browser... issue fixed

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import {Dashboard, Home, RootLayout} from './pages';
 import { ToastContainer } from 'react-toastify';
 import { ErrorPage } from './components';
+import { useEffect } from 'react';
 function App() {
     const router = createBrowserRouter([
         {
@@ -21,6 +22,25 @@ function App() {
             ]
         }
     ]);
+
+    useEffect(() => {
+        var isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
+        var safariVersionMatch = navigator.userAgent.match(/Version\/(\d+)\.(\d+)/);14.1
+      console.log(safariVersionMatch)
+        if (isSafari && safariVersionMatch) {
+            var majorVersion = parseInt(safariVersionMatch[1], 10); 14
+            var minorVersion = parseInt(safariVersionMatch[2], 10); 1
+      
+            var isOldSafari = (majorVersion < 14) || (majorVersion === 14 && minorVersion < 2);
+            console.log(majorVersion, minorVersion, isOldSafari,'Browser Version')
+            if (isOldSafari) {
+                document.body.classList.add("safari_version_less_14_2");
+            } else {
+                console.log("This browser Not Safari || Safari 14.2+");
+            }
+        }
+    }, [])
+   
     return (
     <> 
      <RouterProvider router={router}/>
